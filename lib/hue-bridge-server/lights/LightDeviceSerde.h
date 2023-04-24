cc7dbe96521b3ca40e83385bf0679296894ca421
hmagarotto <magarotto@gmail.com>
2023-04-24 03:16:08 -0300

    Devices serializer/deserializer

--- lib/hue-bridge-server/lights/LightDeviceSerde.h
+++ lib/hue-bridge-server/lights/LightDeviceSerde.h
@@ -3,7 +3,37 @@
 
 #pragma once
 
+#include "LightDevice.h"
 #include <ArduinoJson.hpp>
 
+
+bool convertToJson(const LightDevice& src, JsonVariant dst) {
+    dst["type"] = src.type;
+    dst["name"] = src.name;
+    dst["modelid"] = src.modelid;
+    dst["productname"] = src.productname;
+    dst["uniqueid"] = src.uniqueid;
+    dst["manufacturername"] = src.manufacturername;
+    dst["swversion"] = src.swversion;
+
+    dst["swupdate"]["state"] = src.swupdate.state;
+    dst["swupdate"]["lastinstall"] = src.swupdate.lastinstall;
+
+    dst["capabilities"]["certified"] = src.capabilities.certified;
+    dst["capabilities"]["control"]["mindimlevel"] = src.capabilities.control.mindimlevel;
+    dst["capabilities"]["control"]["maxlumen"] = src.capabilities.control.maxlumen;
+    dst["capabilities"]["control"]["ct"]["min"] = src.capabilities.control.ct.min;
+    dst["capabilities"]["control"]["ct"]["max"] = src.capabilities.control.ct.max;
+    dst["capabilities"]["streaming"]["renderer"] = src.capabilities.streaming.renderer;
+    dst["capabilities"]["streaming"]["proxy"] = src.capabilities.streaming.proxy;
+
+    dst["config"]["archetype"] = src.config.archetype;
+    dst["config"]["function"] = src.config.function;
+    dst["config"]["direction"] = src.config.direction;
+    dst["config"]["startup"]["mode"] = src.config.startup.mode;
+    dst["config"]["startup"]["configured"] = src.config.startup.configured;
+
+    return true;
+}
 
 #endif
--- lib/hue-bridge-server/lights/LightStateSerde.h
+++ lib/hue-bridge-server/lights/LightStateSerde.h
@@ -3,20 +3,23 @@
 
 #pragma once
 
-#include <ArduinoJson.hpp>
 #include "LightState.h"
+#include <ArduinoJson.h>
 
 bool convertToJson(const LightState& src, JsonVariant dst) {
     dst["on"] = src.on;
     dst["bri"] = src.bri;
-    dst["colormode"] = src.colormode;
-    if (strcmp(src.colormode, "ct") == 0) {
-        dst["ct"] = src.ct;
-    } else if (strcmp(src.colormode, "hs") == 0) {
-        dst["hue"] = src.hue;
-        dst["sat"] = src.sat;
-    } else if (strcmp(src.colormode, "xy") == 0) {
-        dst["xy"] = src.xy;
+    if (src.colormode && strlen(src.colormode)) {
+        dst["colormode"] = src.colormode;
+        if (strcmp(src.colormode, "ct") == 0) {
+            dst["ct"] = src.ct;
+        } else if (strcmp(src.colormode, "hs") == 0) {
+            dst["hue"] = src.hue;
+            dst["sat"] = src.sat;
+        } else if (strcmp(src.colormode, "xy") == 0) {
+            dst["xy"][0] = src.xy[0];
+            dst["xy"][1] = src.xy[1];
+        }
     }
     dst["alert"] = src.alert;
     dst["mode"] = src.mode;
@@ -27,7 +30,7 @@ bool convertToJson(const LightState& src, JsonVariant dst) {
 void updateFromJson(JsonVariantConst src, LightState& dst) {
     dst.on = src["on"];
     dst.bri = src["bri"];
-    dst.colormode = src["colormode"];
+    // dst.colormode = src["colormode"];
 
     if (strcmp(dst.colormode, "ct") == 0) {
         dst.ct = src["ct"];
@@ -39,8 +42,8 @@ void updateFromJson(JsonVariantConst src, LightState& dst) {
         dst.xy[1] = src["xy"][1];
     }
 
-    dst.alert = src["alert"];
-    dst.mode = src["mode"];
+    // dst.alert = src["alert"];
+    // dst.mode = src["mode"];
     dst.reachable = src["reachable"];
 }
 
