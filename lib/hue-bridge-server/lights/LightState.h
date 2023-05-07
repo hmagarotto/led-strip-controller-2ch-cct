4d9b1114a18509a4e39e74e6ef902ea0359e794f
hmagarotto <magarotto@gmail.com>
2023-05-07 00:58:29 -0300

    Change default light state

--- lib/hue-bridge-server/lights/LightState.h
+++ lib/hue-bridge-server/lights/LightState.h
@@ -8,11 +8,11 @@
 struct LightState
 {
     bool on = false;
-    uint8_t bri = 1;
+    uint8_t bri = 52;
     uint16_t hue = 0;
     uint8_t sat = 0;
     float xy[2] = {0, 0};
-    uint16_t ct = 153;
+    uint16_t ct = 284;
     char colormode[3] = "ct";
     char alert[16] = "none";
     char mode[16] = "homeautomation";
