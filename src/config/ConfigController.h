b36701b5e01d5e9bc1e79b1bb4e7750c7a18063e
hmagarotto <magarotto@gmail.com>
2023-05-13 23:58:40 -0300

    Add doxygen comments

--- src/config/ConfigController.h
+++ src/config/ConfigController.h
@@ -12,14 +12,47 @@ public:
     ConfigController(AsyncWebServer* server);
     ~ConfigController();
 
+    /**
+     * @brief WebServer routes for config API
+     * @param m WiFiMode_t
+     */
     void setup();
+
+    /**
+     * @brief Serialize config to JSON string
+     * @return config JSON string
+     */
     String serialize();
+
+    /**
+     * @brief Deserialize JSON string into config data
+     * @param json JSON string
+     */
     void deserialize(const String& json);
+
+    /**
+     * @brief Load config from filesystem
+     */
     void load();
+
+    /**
+     * @brief Save config to filesystem
+     */
     void save();
 
+    /**
+     * @brief Get config struct
+     */
     const Config::DTO::Config& getConfig();
+
+    /**
+     * @brief Get light config struct
+     */
     const Config::DTO::Light& getLightConfig(size_t index);
+
+    /**
+     * @brief Get switch config struct
+     */
     const Config::DTO::Switch& getSwitchConfig(size_t index);
 
 private:
